88edf6e84b210cefba44d3d809b07cc433cf6316
Nandhini3124 <nandhini010323@gmail.com>
2025-05-29 12:32:41 +0530

    Add files via upload

---
+++ java/javascript.js
@@ -0,0 +1,13 @@
+function submit(){
+    var a=document.getElementById("first name").value;
+    document.getElementById("ca").innerHTML=a;
+    var b=document.getElementById("middle name").value;
+    document.getElementById("acc").innerHTML=b;
+    var c=document.getElementById("last name").value;
+    document.getElementById("com").innerHTML=c;
+    var d=document.getElementById("age").value;
+    document.getElementById("nandhu").innerHTML=d;
+    var e=document.getElementById("phone number").value;
+    document.getElementById("chocolate").innerHTML=e;
+
+}
